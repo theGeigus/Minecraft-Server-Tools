d17b89a1609b954f127631757f0f48616246b4f2
theGeigus <theGeigus@gmail.com>
2023-04-14 21:27:38 -0500

    Rewrote most of the README, cause it was pretty bad before.

--- stop_server.sh
+++ stop_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Change directory and import variables
-cd "$(dirname "${BASH_SOURCE[0]}")"
+cd "$(dirname "${BASH_SOURCE[0]}")" ||  echo "Something broke, could not find directory?"
 source ./config_server.sh
 
 #--- STOP SERVER ---
