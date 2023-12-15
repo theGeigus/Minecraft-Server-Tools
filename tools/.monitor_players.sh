1ea9396e5c246e92e6b9030993311250425bcb90
Geigus <theGeigus@gmail.com>
2023-12-14 23:12:01 -0600

    Mixing some things around

--- tools/.monitor_players.sh
+++ tools/.monitor_players.sh
@@ -41,21 +41,12 @@ setStatus -1
 # Loop while server is running
 while [ "$(screen -ls | grep  -o "$SERVER_NAME")" == "$SERVER_NAME" ]
 do
-	# Wait for log update, if player connects set day and weather cycle to true
+	# Wait for log update
 	inotifywait -qq -e MODIFY .server.log
-    line="$(tail -1 .server.log)"
-	if [ "$(echo "$line" | grep -o 'Player Spawned:')" == 'Player Spawned:' ] || [ "$(echo "$line" | grep -o 'joined the game')" == "joined the game" ]
-	then
-        [ "${EDITION^^}" == "BEDROCK" ] && player_name=$(echo "$line"| grep -n -o ': .* xuid' | awk '{ print substr($0, 5, length($0)-9) }')
-		[ "${EDITION^^}" == "JAVA" ] && player_name=$(echo "$line" | grep -n -o ': .* joined the game' | awk '{ print substr($0, 5, length($0)-20) }')
-
-        echo "$player_name" >> .server.log
-        grep -q "$player_name" .playedToday 2> /dev/null || echo "$player_name" >> .playedToday
-        setStatus 1
-		./announce_server.sh -p "$player_name"
+    line="$(tail -3 .server.log)"
 
     # If player disconnects, check for remaining players
-	elif [ "$(echo "$line" | grep -o 'Player disconnected:')" == "Player disconnected:" ] || [ "$(echo "$line" | grep -o 'left the game')" == "left the game" ]
+    if [ "$(echo "$line" | grep -o 'Player disconnected:')" == "Player disconnected:" ] || [ "$(echo "$line" | grep -o 'left the game')" == "left the game" ]
     then
 
         screen -Rd "$SERVER_NAME" -X stuff "list\r"
@@ -66,6 +57,17 @@ do
         then
             setStatus 0
         fi
+
+    # If player connects set day and weather cycle to true
+	elif [ "$(echo "$line" | grep -o 'Player Spawned:')" == 'Player Spawned:' ] || [ "$(echo "$line" | grep -o 'joined the game')" == "joined the game" ]
+	then
+        [ "${EDITION^^}" == "BEDROCK" ] && player_name=$(echo "$line"| grep -n -o ': .* xuid' | awk '{ print substr($0, 5, length($0)-9) }')
+		[ "${EDITION^^}" == "JAVA" ] && player_name=$(echo "$line" | grep -n -o ': .* joined the game' | awk '{ print substr($0, 5, length($0)-20) }')
+
+        echo "$player_name" >> .server.log
+        grep -q "$player_name" .playedToday 2> /dev/null || echo "$player_name" >> .playedToday
+        setStatus 1
+		./announce_server.sh -p "$player_name"
 
     # Case for if server crashes, this is how Java Edition crashes print out... Add Bedrock's format later
     elif [ "$(echo "$line" | grep -o 'This crash report has been saved')" == "This crash report has been saved" ]
